01944f47c2eac94d0cdd15418b9a794f03fa1f5e
Blake Lassiter <balassit@ncsu.edu>
2016-11-30 17:14:39 -0500

    remove fragment java classes

--- app/src/main/java/com/example/blake/greenbeans/Recipe.java
+++ app/src/main/java/com/example/blake/greenbeans/Recipe.java
@@ -1,5 +1,7 @@
 package com.example.blake.greenbeans;
 
+import android.media.Image;
+
 import java.util.ArrayList;
 
 /**
@@ -14,16 +16,29 @@ public class Recipe {
     ArrayList<String> ingredients;
     ArrayList<String> equipment;
     ArrayList<String> directions;
+    int rating;
+    String notes;
+    Image img;
 
     public Recipe() {
-
+        this.title = null;
+        this.description = null;
+        this.time = null;
+        this.rating = 0;
+        this.notes = null;
+        this.img = null;
+        this.ingredients = new ArrayList<>();
+        this.equipment = new ArrayList<>();
+        this.directions = new ArrayList<>();
     }
 
-
-    public Recipe(String title, String description, String time, ArrayList<String> ingredients, ArrayList<String> equipment, ArrayList<String> directions) {
+    public Recipe(String title, String description, String time, String notes, int rating, Image img, ArrayList<String> ingredients, ArrayList<String> equipment, ArrayList<String> directions) {
         this.title = title;
         this.description = description;
         this.time = time;
+        this.notes = notes;
+        this.rating = rating;
+        this.img = img;
         this.ingredients = ingredients;
         this.equipment = equipment;
         this.directions = directions;
