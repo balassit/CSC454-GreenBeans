76754c4b57dd64f323706e8abf47665291a2e2d9
Blake Lassiter <balassit@ncsu.edu>
2016-12-02 16:59:32 -0500

    ingredient object created

--- app/src/main/java/com/example/blake/greenbeans/Ingredient.java
+++ app/src/main/java/com/example/blake/greenbeans/Ingredient.java
@@ -1,26 +1,40 @@
 package com.example.blake.greenbeans;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by blake on 11/30/16.
  */
 
-public class Ingredient {
-    private double quantity;
+public class Ingredient implements Parcelable {
+    private String quantity;
     private String unit;
     private String name;
 
-    public Ingredient(double quantity, String unit, String name) {
-        this.quantity = quantity;
-        this.unit = unit;
-        this.name = name;
+    private int mData;
+
+    /* everything below here is for implementing Parcelable */
+
+    // 99.9% of the time you can just ignore this
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
-    public double getQuantity() {
-        return quantity;
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeStringArray(new String[] {this.quantity,
+                this.quantity,
+                this.name,
+                this.unit});
     }
 
-    public void setQuantity(double quantity) {
+
+    public Ingredient(String quantity, String name, String unit) {
         this.quantity = quantity;
+        this.name = unit;
+        this.unit = name;
     }
 
     public String getUnit() {
@@ -38,4 +52,39 @@ public class Ingredient {
     public void setName(String name) {
         this.name = name;
     }
+
+    public double getQuantity() {
+        if(quantity == null){
+            return 0;
+        }
+        return Integer.parseInt(quantity);
+    }
+
+    public void setQuantity(double quantity) {
+        if(quantity > 0) {
+            this.quantity = Double.toString(quantity);
+        }
+    }
+
+    // Parcelling part
+    public Ingredient(Parcel in){
+        String[] data = new String[3];
+
+        in.readStringArray(data);
+        this.quantity = data[0];
+        this.name = data[1];
+        this.unit = data[2];
+    }
+
+
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public Ingredient createFromParcel(Parcel in) {
+            return new Ingredient(in);
+        }
+
+        public Ingredient[] newArray(int size) {
+            return new Ingredient[size];
+        }
+    };
 }
